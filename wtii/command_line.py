3050b2d71664ddf24d4aff2d2c96c8cc50af1b2e
Janis Lesinskis <janis@lesinskis.com>
2019-10-04 21:34:26 +1000

    Handle locations argument

--- wtii/command_line.py
+++ wtii/command_line.py
@@ -1,2 +1,13 @@
-def main():
+import argparse
+from typing import List
+
+parser = argparse.ArgumentParser(description='Find the time where other people are.')
+parser.add_argument('--locations', action='store_true', help="List all locations")
+
+def locations() -> List:
     raise NotImplementedError
+
+def main() -> None:
+    args = parser.parse_args()
+    if args.locations is True:
+        print(locations())
