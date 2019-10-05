5a9cd98d655f6d57ba7259fea5237f31e2bf81d2
Janis <janis@customprogrammingsolutions.com>
2019-10-05 15:32:04 +1000

    Create verbose flag

--- wtii/command_line.py
+++ wtii/command_line.py
@@ -8,6 +8,8 @@ parser.add_argument('--locations', action='store_true', help="List all locations
 parser.add_argument('-d', '--delta', help="Find a time delta from now")
 parser.add_argument('-n', '--now', help="Current time (plus offset)")
 
+parser.add_argument('-v', '--verbose', action='store_true', help="Enable verbose mode")
+
 
 def locations() -> Dict[str, str]:
     """Return a dict of the locations of interest along with their time zones"""
@@ -19,7 +21,9 @@ def main() -> None:
     if args.locations is True:
         print(locations())
         return None
-    utc_now = arrow.utcnow()
-    print("UTC", utc_now)
+
+    if args.verbose is True:
+        utc_now = arrow.utcnow()
+        print("UTC", utc_now)
     for loc, tz_loc in locations().items():
         print(loc, utc_now.to(tz_loc))
